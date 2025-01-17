85e152ea5d7394294677624f1ed54a49eb4ebb38
Victor Diogo <victordiogo2801@gmail.com>
2025-01-17 13:03:26 -0300

    Questão bactérias de 2024 (nível jr, fase 1)

---
+++ bacterias.py
@@ -0,0 +1,21 @@
+capacidade = int(input())
+multiplicador = int(input())
+
+bacterias = 1
+dia = 0
+while bacterias <= capacidade:
+    bacterias = bacterias * multiplicador
+    dia = dia + 1
+print(dia-1)
+
+
+
+
+
+
+
+
+
+
+
+
